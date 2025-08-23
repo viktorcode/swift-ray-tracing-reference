fedfc33c3941d335792c1c2333dc6a4cb41fd6e5
Viktor Chernikov <viktorcode@icloud.com>
2025-08-23 10:52:43 +0200

    Initial Commit

---
+++ Ray Reference/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ray Reference
+//
+//  Created by Viktor Chernikov on 23.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ray Reference/Ray_ReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ray_ReferenceApp.swift
+//  Ray Reference
+//
+//  Created by Viktor Chernikov on 23.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Ray_ReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
